ecda09670c9e6b10f5e7495f28afd93a791317f5
MaxGuidry <mguidry7@outlook.com>
2017-04-26 17:49:33 -0500

    lit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputField inputField;
+    private Vector2 position;
+    void Start()
+    {
+        inputField.onEndEdit.AddListener(updateinput);
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void updateinput(string f)
+    {
+        if (inputField.text.ToUpper() == "W")
+        {
+            position += new Vector2(-1, 0);
+        }
+        else if (inputField.text.ToUpper() == "E")
+        {
+            position += new Vector2(1, 0);
+        }
+        else if (inputField.text.ToUpper() == "N")
+        {
+            position += new Vector2(0, 1);
+        }
+        else if (inputField.text.ToUpper() == "S")
+        {
+            position += new Vector2(0, -1);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController2 : MonoBehaviour {
+
+    private Vector2 position;
+    public Text text;
+	// Use this for initialization
+	void Start ()
+    {
+        position = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            position += new Vector2(-1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            position += new Vector2(1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            position += new Vector2(0, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            position += new Vector2(0, -1);
+        }
+        text.text = position.ToString();
+    }
+}
