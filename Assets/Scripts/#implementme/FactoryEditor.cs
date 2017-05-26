c9d7df3b73625cc6e40d7a77cf35b526f856551f
MaxGuidry <mguidry7@outlook.com>
2017-05-26 11:28:22 -0500

    Factory

---
+++ Assets/Scripts/#implementme/FactoryEditor.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
---
+++ Assets/Scripts/#implementme/Scooter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scooter : IFactory {
+    public void Drive(int i)
+    {
+        Debug.Log("Driving with speed: " + i.ToString());
+    }
+
+   
+}
